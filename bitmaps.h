3033f2d8ba61c85cb07f7e821e5bfcd44c5e9228
Jezzamonn <jezzamonn@gmail.com>
2016-06-10 19:41:52 +1000

    Adding graphics for the pieces

---
+++ bitmaps.h
@@ -0,0 +1,11 @@
+#ifndef BITMAPS_H
+#define BITMAPS_H
+
+#include <avr/pgmspace.h>
+
+extern const unsigned char king1[];
+extern const unsigned char king2[];
+extern const unsigned char piece1[];
+extern const unsigned char piece2[];
+
+#endif // BITMAPS_H
